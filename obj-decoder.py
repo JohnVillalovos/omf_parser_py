40ea8ecdf7c5fd881cd19cb76b2b14a8a3529d2d
John L. Villalovos <john@sodarock.com>
2020-06-15 09:49:05 -0700

    Initial commit

---
+++ obj-decoder.py
@@ -0,0 +1,433 @@
+#!/usr/bin/python3 -ttu
+# vim: ai ts=4 sts=4 et sw=4
+
+# Copyright (c) 2019 John L. Villalovos
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Documentation on the Relocatable Object Module Format (OMF) is in the
+# document "Tool Interface Standard (TIS) Portable Formats Specification
+# Version 1.1" located at:
+# https://refspecs.linuxfoundation.org/elf/TIS1.1.pdf
+
+import copy
+import os
+import pprint
+import sys
+from typing import Any, Callable, Dict, List, Optional, Tuple
+
+
+def main():
+    with open("numon.obj", "rb") as in_file:
+        data = in_file.read()
+
+    # pprint.pprint(parse_object_module(data))
+    parse_object_module(data)
+
+
+def extract_records(object_module_data: bytes) -> List[bytes]:
+    """Extract each record from the data from an object file"""
+    records = []
+    data = copy.copy(object_module_data)
+    while data:
+        # Record length is stored in offset 1 & 2 of the record as a 16 bit
+        # little-endian integer
+        record_length = int.from_bytes(data[1:2], byteorder="little", signed=False)
+        record = data[: record_length + 3]
+        records.append(record)
+        data = data[3 + record_length :]
+    return records
+
+
+def parse_object_module(object_module_data: bytes):
+    raw_records = extract_records(object_module_data)
+    records = []
+
+    for raw_record in raw_records:
+        record_class = create_record(record_data=raw_record)
+        print(record_class)
+        print()
+        record_class = Record(record_data=raw_record)
+
+        record_layout = None
+        record: Dict[str, Any] = dict()
+
+#        if record_class.record_type in RECORD_TYPES:
+#            record_layout = RECORD_TYPES[record_class.record_type]
+#            rec_type_str = record_layout.description
+#        else:
+#            print("Unknown header", hex(record_class.record_type))
+#            rec_type_str = "Unknown header"
+#            raise ValueError("Unknown header: {}".format(hex(record_class.record_type)))
+#
+#        record = {
+#            "record_type": hex(record_class.record_type),
+#            "record_type_str": rec_type_str,
+#        }
+#
+#        # The record data without the record type and record length
+#        record_data = record_class.get_record_payload()
+#
+#        record["data"] = record_data
+#
+#        # Every record has a checksum as the last byte
+#        record["checksum"] = record_class.checksum
+#
+#        if record_layout and record_layout.parser:
+#            parsed_data = record_layout.parser(record_data)
+#            print("Parsed data:", parsed_data)
+#        if record_layout and record_layout.has_name:
+#            if record_layout.multiple_names:
+#                record["names"] = get_multiple_names(record_data)
+#            else:
+#                name = get_name(record_data)
+#                record["name"] = name
+#                print("Name:", name)
+#
+#        records.append(record)
+
+    return records
+
+
+def get_name(data: bytes):
+    name_length = data[0]
+    name = data[1 : name_length + 1]
+    return os.fsdecode(name)
+
+
+def get_multiple_names(data: bytes):
+    names = []
+    while len(data) > 1:
+        name = get_name(data)
+        names.append(name)
+        data = data[1 + len(name) :]
+    assert len(data) == 1
+    return names
+
+
+def parse_record_80_theadr(data):
+    """Parse a T-Module Header Record"""
+    pass
+
+
+class Record:
+    def __init__(self, *, record_data: bytes):
+        self.record_data = record_data
+
+        self.record_type = record_data[0]
+        self.record_type_desc = "Unknown"
+        if self.record_type in RECORD_TYPES:
+            self.record_type_desc = RECORD_TYPES[self.record_type].description
+        self.record_length = int.from_bytes(
+            record_data[1:2], byteorder="little", signed=False
+        )
+        self._validate_record()
+        self.checksum: int = self.record_data[-1]
+        self._payload = self.get_record_payload()
+
+    def get_record_payload(self):
+        # Return the contents of the recrd data without the record type or
+        # record length
+        return copy.copy(self.record_data[3:])
+
+    def _validate_record(self):
+        if len(self.record_data) != 3 + self.record_length:
+            raise ValueError(
+                "ERROR: Record length incorrect. Expected {} is {}".format(
+                    3 + self.record_length, len(self.record_data)
+                )
+            )
+        # Check the checksum
+        chk_sum = 0
+        for character in self.record_data:
+            chk_sum += character
+        if chk_sum % 256 != 0:
+            print("ERROR: Checksum failure")
+            print(data)
+            print("chk_sum:", chk_sum)
+            print("chk_sum:", chk_sum % 256)
+            sys.exit()
+
+    def _get_name(self, data) -> str:
+        # This should only be called if sure have a name
+        name_length = data[0]
+        name = data[1 : name_length + 1]
+        return os.fsdecode(name)
+
+    def __str__(self, *, extra: str = ""):
+        out_string = f"Record: <record_type: 0x{self.record_type:X},"
+        out_string += " record_type_desc: {!r},".format(self.record_type_desc)
+        out_string += extra
+        out_string += " data: {!r},".format(self._payload)
+
+        out_string += ">"
+        return out_string
+
+    def is_theader(self):
+        return self.record_type == THEADR
+
+    def is_lnames(self):
+        return self.record_type == LNAMES
+
+    def get_index_value(self, offset) -> Tuple[int, int]:
+        index_1 = self._payload[offset]
+        if index_1 & 0x80:
+            index = (index_1 & 0x74) * 0x100 + self._payload[offset + 1]
+            size = 2
+        else:
+            index = index_1
+            size = 1
+        return (index, size)
+
+
+class THeaderRecord(Record):
+    def __init__(self, record_data: bytes):
+        super().__init__(record_data=record_data)
+        assert self.record_type == THEADR
+        self.name = self._get_name(data=self._payload)
+
+    def __str__(self):
+        out_str = " name: {!r},".format(self.name)
+        return super().__str__(extra=out_str)
+
+
+class LNamesRecord(Record):
+    def __init__(self, record_data: bytes):
+        super().__init__(record_data=record_data)
+        assert self.record_type == LNAMES
+        self.names = get_multiple_names(data=self._payload)
+
+    def __str__(self):
+        out_str = " names: {!r},".format(self.names)
+        return super().__str__(extra=out_str)
+
+
+class SegdefRecord(Record):
+    def __init__(self, record_data: bytes):
+        super().__init__(record_data=record_data)
+        assert self.record_type in (SEGDEF, SEGDEF_32)
+        # 0x98 is 16 bit (2 bytes), 0x99 is 32 bit (4 byte). For the Segment
+        # Length field
+        self.bit_32: bool = (self.record_type == 0x99)
+        self.__parse_segment_attributes()
+        self.__parse_segment_length()
+        self.__parse_indexes()
+
+    def __parse_segment_attributes(self):
+        attributes = self._payload[0]
+        self.attributes = attributes
+        self.have_attribute_frame = False
+        self.attribute_frame_number = 0
+        self.attribute_offset = 0
+
+        self.alignment = attributes >> 5
+        if self.alignment == 0:
+            self.have_attribute_frame = True
+            self.frame_number = int.from_bytes(
+                self._payload[1:2], byteorder="little", signed=False
+            )
+            self.frame_offset: int = self._payload[3]
+
+        self.alignment_desc = {
+            0: "Absolute segment.",
+            1: "Relocatable, byte aligned.",
+            2: "Relocatable, word (2-byte, 16-bit) aligned.",
+            3: "Relocatable, paragraph (16-byte) aligned.",
+            4: "Relocatable, aligned on a page boundary.",
+            5: "Relocatable, aligned on a double word (4-byte) boundary.",
+            6: "Not supported",
+            7: "Not supported",
+        }[self.alignment]
+
+        self.combination = (attributes >> 2) & 0x7
+        self.combination_desc = {
+            0: "Private. Do not combine with any other program segment.",
+            1: "Reserved.",
+            2: (
+                "Public. Combine by appending at an offset that meets the alignment "
+                "requirement."
+            ),
+            3: "Reserved.",
+            4: (
+                "Public. Combine by appending at an offset that meets the alignment "
+                "requirement."
+            ),
+            5: "Stack. Combine as for C=2. This combine type forces byte alignment.",
+            6: "Common. Combine by overlay using maximum size.",
+            7: (
+                "Public. Combine by appending at an offset that meets the alignment "
+                "requirement."
+            ),
+        }[self.combination]
+
+        self.big = (attributes >> 1) & 0x01
+        self.bit_field = attributes & 0x01
+
+    def __parse_segment_length(self):
+        offset = 1
+        if self.have_attribute_frame:
+            offset += 3
+        end_offset = offset + 1
+        if self.bit_32:
+            end_offset = offset + 3
+
+        self.segment_length = int.from_bytes(
+            self._payload[offset:end_offset], byteorder="little", signed=False
+        )
+
+    def __parse_indexes(self):
+        offset = 3
+        if self.have_attribute_frame:
+            offset += 3
+        if self.bit_32:
+            offset += 2
+        self.segment_name_index, add_offset = self.get_index_value(offset)
+        print("segment name index", self.segment_name_index)
+
+        offset += add_offset
+        self.class_name_index, add_offset = self.get_index_value(offset)
+        print("class name index", self.class_name_index)
+
+        offset += add_offset
+        self.overlay_name_index, add_offset = self.get_index_value(offset)
+        print("overlay name index", self.overlay_name_index)
+
+    def __str__(self):
+        extra = " alignment: {}, alignment_desc: {!r}".format(
+            self.alignment, self.alignment_desc
+        )
+        if self.have_attribute_frame:
+            extra += "attribute_frame_number: {}, attribute_offset: {}".format(
+                self.attribute_frame_number, self.attribute_offset
+            )
+        extra += " combination: {}, combination_desc: {!r},".format(
+            self.combination, self.combination_desc
+        )
+        extra += " big: {},".format(1 if self.big else 0)
+        extra += " bit_field: {},".format(1 if self.bit_field else 0)
+        extra += " seg_name_idx: {},".format(self.segment_name_index)
+        extra += " cls_name_idx: {},".format(self.class_name_index)
+        extra += " ovr_name_idx: {},".format(self.overlay_name_index)
+
+
+        return super().__str__(extra=extra)
+
+
+class GrpdefRecord(Record):
+    def __init__(self, record_data: bytes):
+        super().__init__(record_data=record_data)
+        assert self.record_type == GRPDEF
+
+    def __str__(self):
+        out_str = ""
+        return super().__str__(extra=out_str)
+
+
+
+class RecordLayout:
+    def __init__(
+        self,
+        *,
+        record_type: int,
+        description: str,
+        has_name: bool,
+        parser: Optional[Callable] = None,
+        multiple_names: bool = False,
+    ):
+        self.record_type = record_type
+        self.description = description
+        self.has_name = has_name
+        self.multiple_names = multiple_names
+        self.parser = parser
+
+        if multiple_names and not has_name:
+            raise ValueError(
+                "Error: Sepcified 'multiple_names' but did not specify 'has_name'"
+            )
+
+
+def create_record(record_data: bytes) -> Record:
+    base_record = Record(record_data=record_data)
+    record_type = base_record.record_type
+
+    if record_type == THEADR:
+        return THeaderRecord(record_data)
+    if record_type == LNAMES:
+        return LNamesRecord(record_data)
+    if record_type in (SEGDEF, SEGDEF_32):
+        return SegdefRecord(record_data)
+    if record_type == GRPDEF:
+        return GrpdefRecord(record_data)
+
+    return base_record
+
+
+# Constants
+THEADR = 0x80
+LHEADR = 0x82
+RHEADR = 0x63
+MODEND = 0x8A
+EXTDEF = 0x8C
+PUBDEF = 0x90
+LNAMES = 0x96
+SEGDEF = 0x98
+GRPDEF = 0x9A
+LEDATA = 0xA0
+# 32 Bit version of SEGDEF
+SEGDEF_32 = 0x99
+
+
+RECORD_TYPES = {
+    THEADR: RecordLayout(
+        record_type=THEADR,
+        description="T-Module Header Record",
+        has_name=True,
+        parser=parse_record_80_theadr,
+    ),
+    LNAMES: RecordLayout(
+        record_type=LNAMES,
+        description="List of Names Record",
+        has_name=True,
+        multiple_names=True,
+    ),
+    SEGDEF: RecordLayout(
+        record_type=SEGDEF, description="Segment Definition Record", has_name=False,
+    ),
+    GRPDEF: RecordLayout(
+        record_type=GRPDEF, description="Group Definition Record", has_name=False,
+    ),
+    PUBDEF: RecordLayout(
+        record_type=PUBDEF,
+        description="Public Names Definition Record",
+        has_name=False,
+    ),
+    LEDATA: RecordLayout(
+        record_type=LEDATA,
+        description="Logical Enumerated Data Record",
+        has_name=False,
+    ),
+    MODEND: RecordLayout(
+        record_type=MODEND, description="Module End Record", has_name=False,
+    ),
+    EXTDEF: RecordLayout(
+        record_type=EXTDEF,
+        description="External Names Definition Record",
+        has_name=False,
+    ),
+}
+
+RECORD_TYPES_WITH_NAMES = {THEADR, LNAMES}
+
+
+if "__main__" == __name__:
+    sys.exit(main())
